35cac7c0f7d82923d4df8730e75d3a411c926983
OisifCode <clement.rollot71@gmail.com>
2023-05-04 17:09:44 +0200

    UPDATE: Home Page

--- src/Pages/WelcomePages/Home/HomePage.jsx
+++ src/Pages/WelcomePages/Home/HomePage.jsx
@@ -1,6 +1,25 @@
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
-import { Container, Typography } from '@mui/material';
+import { Grid, Card, Container, Typography, Button } from '@mui/material';
+import Iconify from '../../../components/iconify/Iconify';
+
+import { styled } from '@mui/material/styles';
+
+// ----------------------------------------------------------------------
+
+const StyledCard = styled('div')(({ theme }) => ({
+  margin: 'auto',
+  display: 'flex',
+  borderRadius: '10px',
+  alignItems: 'center',
+  width: theme.spacing(50),
+  height: theme.spacing(8),
+  justifyContent: 'center',
+  marginBottom: theme.spacing(3),
+  backgroundColor: 'red'
+}));
+
+// ----------------------------------------------------------------------
 
 export default function HomePage() {
 
@@ -12,9 +31,67 @@ export default function HomePage() {
       </title>
     </Helmet>
    <Container>
+    
       <h1>
-        FREEDASH CROLLOT
+        FreeDash
       </h1>
+
+      <Typography 
+        variant="body1"
+      >
+        Soyez en total contrôle, en permanence.<br/>
+        Gérez vos missions à la perfection avec FreeDash.
+      </Typography>
+
+      <Link 
+        to="/register"
+      >
+          <Button
+            variant="contained"
+            startIcon={<Iconify icon="bi:lightning-charge-fill" />}
+            >
+            Get started
+          </Button>
+      </Link>
+
+        
+      <Container>
+        <Grid container spacing={3}>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <StyledCard>
+                <Typography variant="button">
+                  Photo
+                </Typography>
+            </StyledCard>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <StyledCard>
+                <Typography variant="button">
+                  Photo
+                </Typography>
+            </StyledCard>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <StyledCard>
+                <Typography variant="button">
+                  Photo
+                </Typography>
+            </StyledCard>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <StyledCard>
+                <Typography variant="button">
+                  Photo
+                </Typography>
+            </StyledCard>
+          </Grid>
+          
+        </Grid>
+      </Container>
 
 
       <ul>
