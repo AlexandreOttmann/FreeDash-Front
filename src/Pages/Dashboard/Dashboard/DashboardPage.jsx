ba23fe54a07812a1631cea95429585dfee48103a
AlexandreOttmann <ottmann.alex@gmail.com>
2023-05-16 15:08:33 +0200

    FEAT: Dashboard Done | Display data from api in each card and charts

--- src/Pages/Dashboard/Dashboard/DashboardPage.jsx
+++ src/Pages/Dashboard/Dashboard/DashboardPage.jsx
@@ -5,33 +5,41 @@ import { Grid, Container, Typography } from '@mui/material';
 // components
 import Iconify from '../../../components/iconify/Iconify';
 
+// sections
 import WelcomeSection from './Sections/WelcomeSection';
 import CurrentMission from './Sections/CurrentMission';
 import AppWidgetSummary from './Sections/AppWidgetSummary';
 import TopClientsSection from './Sections/TopClientsSection';
 import CalendarSection from './Sections/CalendarSection';
+import YearlyRevenueSection from './Sections/YearlyRevenueSection';
+import TotalYearlyRevenue from './Sections/TotalYearlyRevenue';
 //utils
 import { retrieveUserId } from '../../../utils/retrieveUserId';
 import { retrieveUserData } from '../../../store/reducers/user';
 
 import { axiosInstance } from '../../../api/axios'
+import { last } from 'lodash';
 
 
 export default function DashboardPage() {
   const theme = useTheme();
 
   const [missions, setMissions] = useState([]);
-
-  const [topClients, setTopClients] = useState([]) // [ {name: 'client1', missions: 2}, {name: 'client2', missions: 1}
+  const [lastMonths, setLastMonths] = useState([])
+  const [topClients, setTopClients] = useState([])
 
   const [clients, setClients] = useState([]);
-  const [currentMissions, setCurrentMissions] = useState([]); // [  
+  const [currentMissions, setCurrentMissions] = useState([]);
   const [totalClient, setTotalClient] = useState(0);
   const [totalMission, setTotalMission] = useState(0);
 
+
+  const [revenueEvolution, setRevenueEvolution] = useState([]) // array of revenue evolution
+  const [monthlyRevenue, setMonthlyRevenue] = useState([]) // array of revenue evolution  
+
   const [totalMissionNotDeclared, setTotalMissionNotDeclared] = useState(0);
-  const [totalRevenueNotDeclared, setTotalRevenueNotDeclared] = useState(0); //
-  const [revenue, setRevenue] = useState(0);
+  const [totalRevenueNotDeclared, setTotalRevenueNotDeclared] = useState(0);
+  const [totalRevenueDeclared, setTotalRevenueDeclared] = useState(0);
 
   //FETCH 1 : Missions List
   //FETCH 2 : Clients List
@@ -43,14 +51,20 @@ export default function DashboardPage() {
     return currentMissions
   }
   const handleTotalMissionNotDeclared = (missions) => {
-    const notDeclaredMissions = missions.filter(mission => mission.status !== 'Déclarée')
+    const notDeclaredMissions = missions.filter(mission => !mission.declarate)
     return notDeclaredMissions
   }
 
   const handleRevenueNotDeclared = (missions) => {
-    const notDeclaredMissions = missions.filter(mission => mission.status !== 'Déclarée')
+    const notDeclaredMissions = missions.filter(mission => !mission.declarate)
     const revenueNotDeclared = notDeclaredMissions.reduce((acc, mission) => acc + +mission.totalPrice, 0)
     return revenueNotDeclared
+  }
+
+  const handleRevenueDeclared = (missions) => {
+    const declaredMissions = missions.filter(mission => mission.declarate)
+    const revenueDeclared = declaredMissions.reduce((acc, mission) => acc + +mission.totalPrice, 0)
+    return revenueDeclared
   }
 
   const handleTotalClient = (missions) => {
@@ -65,18 +79,74 @@ export default function DashboardPage() {
     const uniqueClients = [...new Set(clients)]
     const topClients = uniqueClients.map(client => {
       const ClientMissions = missions.filter(mission => mission.client === client)
-      return { name: ClientMissions[0].name, missions: ClientMissions.length }
+      console.log(client)
+      return { name: ClientMissions[0].name, missions: ClientMissions.length, }
     })
-
     return topClients
   }
+
+  const handleRevenueEvolution = (missions) => {
+    // get the missions with status "Terminé"
+    const filteredMissions = missions.filter(mission => mission.status !== 'En Cours' && new Date(mission.endDate) > new Date(new Date().setMonth(new Date().getMonth() - 11)))
+
+    filteredMissions.sort((a, b) => new Date(a.endDate) - new Date(b.endDate));
+    console.log('filtered', filteredMissions)
+    const revenueEvolution = new Array(11).fill(0);
+    let previousRevenue = 0;
+    filteredMissions.forEach(mission => {
+      previousRevenue += +mission.totalPrice
+
+      const endDate = new Date(mission.endDate)
+      const monthIndex = Math.abs(new Date().getMonth() + endDate.getMonth());
+      revenueEvolution[monthIndex + 3] = previousRevenue
+    })
+    return revenueEvolution
+  }
+
+  const lastElevenMonths = () => {
+    const dates = [];
+    const today = new Date();
+
+    for (let i = 10; i >= 0; i--) {
+      const month = new Date(today.getFullYear(), today.getMonth() - i);
+      const formattedDate = `${(month.getMonth() + 1).toString().padStart(2, '0')}/${month.getDate().toString().padStart(2, '0')}/${month.getFullYear()}`;
+      dates.push(formattedDate);
+    }
+    console.log(dates)
+    return dates
+  }
+
+
+  const handleMonthlyRevenue = (missions) => {
+    const filteredMissions = missions.filter(mission => mission.status !== 'En Cours' && new Date(mission.endDate) > new Date(new Date().setMonth(new Date().getMonth() - 11)))
+
+    filteredMissions.sort((a, b) => new Date(a.endDate) - new Date(b.endDate));
+    const monthlyRevenue = new Array(11).fill(0);
+
+
+    filteredMissions.forEach(mission => {
+
+
+      const endDate = new Date(mission.endDate)
+      const monthIndex = Math.abs(new Date().getMonth() + endDate.getMonth());
+
+      const revenue = +mission.totalPrice
+      monthlyRevenue[monthIndex + 3] = revenue
+    })
+    console.log(monthlyRevenue)
+    return monthlyRevenue
+  }
+
+
+
+
 
   const getMissions = useCallback(async () => {
     try {
       //! => Change user ID from localStorage
       // const response = await axiosInstance.get(`user/${userId}/mission`);
       const response = await axiosInstance.get('/user/1/mission');
-      // console.log(response.data)
+      console.log(response.data)
 
       // set Mission NUmber
       setTotalMission(response.data.length);
@@ -95,12 +165,20 @@ export default function DashboardPage() {
       const revenueNotDeclaredFromApi = handleRevenueNotDeclared(response.data)
       setTotalRevenueNotDeclared(revenueNotDeclaredFromApi)
 
+      const revenueDeclaredFromApi = handleRevenueDeclared(response.data)
+      setTotalRevenueDeclared(revenueDeclaredFromApi)
+
       const totalClientFromApi = handleTotalClient(response.data)
       setTotalClient(totalClientFromApi)
 
       const topClientsFromApi = handleTopClients(response.data)
       setTopClients(topClientsFromApi)
 
+      const revenueEvolutionFromApi = handleRevenueEvolution(response.data)
+      setRevenueEvolution(revenueEvolutionFromApi)
+
+      const monthlyRevenueFromApi = handleMonthlyRevenue(response.data)
+      setMonthlyRevenue(monthlyRevenueFromApi)
     } catch (error) {
       console.log(error);
     }
@@ -108,6 +186,8 @@ export default function DashboardPage() {
 
   useEffect(() => {
     getMissions();
+    const months = lastElevenMonths()
+    setLastMonths(months)
   }, []);
   //========================================
   //DATA A CALCULER:
@@ -145,13 +225,53 @@ export default function DashboardPage() {
           </Grid>
 
           <Grid item xs={12} sm={6} md={4}>
+            <TopClientsSection clients={topClients} />
+          </Grid>
+
+          <Grid item xs={12} sm={8} md={8}>
+            <YearlyRevenueSection
+              title="Evolution du chiffre d'affaire"
+              subheader="sur les derniers mois"
+              chartLabels={lastMonths}
+              chartData={[
+                {
+                  name: "Chiffre d'affaire mensuel",
+                  type: 'column',
+                  fill: 'solid',
+                  data: monthlyRevenue,
+                },
+                {
+                  name: "Evolution du chiffre d'affaire",
+                  type: 'area',
+                  fill: 'gradient',
+                  data: revenueEvolution
+                },
+              ]} />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={6}>
             <CalendarSection />
           </Grid>
 
+          <Grid item xs={12} sm={6} md={6}>
+            <TotalYearlyRevenue
+              title="Répartition des revenus"
+              chartData={[
+                { label: 'Revenus non déclarés', value: totalRevenueNotDeclared },
+                { label: 'Revenus déclarés', value: totalRevenueDeclared },
 
-          <Grid item xs={12} sm={6} md={4}>
-            <TopClientsSection clients={topClients} />
+              ]}
+              chartColors={[
+                theme.palette.primary.main,
+                theme.palette.info.main,
+                theme.palette.warning.main,
+                theme.palette.error.main,
+              ]}
+            />
           </Grid>
+
+
+
 
 
 
