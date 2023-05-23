9d6785a0e60e4556fb695afd9fea15c83a0f9b3c
Sansdraps <sandra.letessier@gmail.com>
2023-05-23 17:50:08 +0200

    FEAT: ProfilPage with Form

--- src/Pages/Dashboard/Profil/ProfilPage.jsx
+++ src/Pages/Dashboard/Profil/ProfilPage.jsx
@@ -1,10 +1,33 @@
+import { useSelector } from 'react-redux';
+import ProfileForm from './section/ProfileForm';
+import { Container } from '@mui/material';
+import styled from '@emotion/styled';
 
-export default function ProfilPage() {
+const StyledSection = styled('div')(({ theme }) => ({
+  maxWidth: 600,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  boxShadow: theme.customShadows.card,
+}));
+
+const ProfilePage = () => {
+  const profile = useSelector((state) => state.profile);
+
   return (
-   <div>
-      <h1>
-        je suis là page ProfilPage
-      </h1>
+    <div style={{ display: 'flex' }}>
+      <StyledSection>
+        <Container maxWidth="md">
+          <img src="/src/assets/illustrations/settings_illustration.png" alt="login" />
+        </Container>
+      </StyledSection>
+      <div style={{ flex: '1' }}>
+        <Container maxWidth="md">
+          <ProfileForm profile={profile} />
+        </Container>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default ProfilePage;
